bafdf313e56c2c1e27203f14dc1899cf2eca0696
Thomas <2985511+teakimm@users.noreply.github.com>
2022-11-18 04:40:29 -0600

    Style the page add reset functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const reset = document.querySelector(".reset");
 const details = document.querySelector(".game-details");
 const buttonUi = document.querySelector(".button-ui");
 
-rock.addEventListener('click', e => {
+rock.addEventListener("click", e => {
   document.getElementsByClassName("player-choice")[0].src="https://www.dictionary.com/e/wp-content/uploads/2018/09/moai-emoji.png";
   let cpuChoice = getComputerChoice();
   if(cpuChoice === "Rock") {
@@ -19,20 +19,16 @@ rock.addEventListener('click', e => {
   document.getElementsByClassName("score")[0].innerHTML = "Score: " + playerWins + " to " + cpuWins;
   document.getElementsByClassName("rounds")[0].innerHTML = "Rounds Played: " + rounds;
   if(rounds === 5) {
-    const gameResult = document.createElement('p');
     if(playerWins === cpuWins) {
-      gameResult.textContent = "After 5 rounds, you have tied with the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer).';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have tied with the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer).";
     } else if (playerWins > cpuWins) {
-      gameResult.textContent = "After 5 rounds, you have won against the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer). Good job!';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have won against the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer). Good job!";
     } else {
-      gameResult.textContent = "After 5 rounds, you have lost against the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer). Skill issue.';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have lost against the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer). Skill issue.";
     }
   }
 });
-paper.addEventListener('click', e => {
+paper.addEventListener("click", e => {
   document.getElementsByClassName("player-choice")[0].src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f9fb.png";
   let cpuChoice = getComputerChoice(); 
   if(cpuChoice === "Rock") {
@@ -46,21 +42,16 @@ paper.addEventListener('click', e => {
   document.getElementsByClassName("score")[0].innerHTML = "Score: " + playerWins + " to " + cpuWins;
   document.getElementsByClassName("rounds")[0].innerHTML = "Rounds Played: " + rounds;
   if(rounds === 5) {
-    const gameResult = document.createElement('p');
     if(playerWins === cpuWins) {
-      gameResult.textContent = "After 5 rounds, you have tied with the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer).';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have tied with the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer).";
     } else if (playerWins > cpuWins) {
-      gameResult.textContent = "After 5 rounds, you have won against the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer). Good job!';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have won against the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer). Good job!";
     } else {
-      gameResult.textContent = "After 5 rounds, you have lost against the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer). Skill issue.';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have lost against the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer). Skill issue.";
     }
   }
-  
 });
-scissors.addEventListener('click', e => {
+scissors.addEventListener("click", e => {
   document.getElementsByClassName("player-choice")[0].src="https://images.emojiterra.com/google/noto-emoji/v2.034/512px/2702.png"
   let cpuChoice = getComputerChoice();
   if(cpuChoice === "Rock") {
@@ -74,51 +65,56 @@ scissors.addEventListener('click', e => {
   document.getElementsByClassName("score")[0].innerHTML = "Score: " + playerWins + " to " + cpuWins;
   document.getElementsByClassName("rounds")[0].innerHTML = "Rounds Played: " + rounds;
   if(rounds === 5) {
-    const gameResult = document.createElement('p');
     if(playerWins === cpuWins) {
-      gameResult.textContent = "After 5 rounds, you have tied with the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer).';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have tied with the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer).";
     } else if (playerWins > cpuWins) {
-      gameResult.textContent = "After 5 rounds, you have won against the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer). Good job!';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have won against the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer). Good job!";
     } else {
-      gameResult.textContent = "After 5 rounds, you have lost against the computer with a score of " + playerWins + ' (You) to ' + cpuWins + ' (Computer). Skill issue.';
-      details.appendChild(gameResult);
+      document.getElementsByClassName("final-result")[0].innerHTML = "After 5 rounds, you have lost against the computer with a score of " + playerWins + " (You) to " + cpuWins + " (Computer). Skill issue.";
     }
   }
 });
 
+reset.addEventListener("click", e => {
+  playerWins = 0;
+  cpuWins = 0;
+  rounds = 0;
+  document.getElementsByClassName("player-choice")[0].src="https://images.emojiterra.com/twitter/512px/2753.png";
+  document.getElementsByClassName("cpu-choice")[0].src="https://images.emojiterra.com/twitter/512px/2753.png"
+  document.getElementsByClassName("score")[0].innerHTML = "Score:";
+  document.getElementsByClassName("rounds")[0].innerHTML = "Rounds Played:";
+  document.getElementsByClassName("round-result")[0].innerHTML = "Best of 5. Choose rock, paper, or scissors.";
+  document.getElementsByClassName("final-result")[0].innerHTML = "";
+});
+
 function getComputerChoice() {
-  let rps = ['Rock', 'Paper', 'Scissors'];
+  let rps = ["Rock", "Paper", "Scissors"];
   return rps[Math.floor(Math.random() * rps.length)];
 }
+
 let playerWins = 0;
 let cpuWins = 0;
 let rounds = 0;
+
 function playRound(playerSelection, computerSelection) {
   let playerLower = playerSelection.toLowerCase();
   let cpuLower = computerSelection.toLowerCase();
   if(playerSelection === computerSelection) {
     rounds++;
-    return 'It\'s a tie. Both you and the computer chose ' + cpuLower + '.';
+    return "It\'s a tie. Both you and the computer chose " + cpuLower + ".";
     
-  } else if((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-          (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-          (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+  } else if((playerSelection === "Rock" && computerSelection === "Scissors") ||
+          (playerSelection === "Paper" && computerSelection === "Rock") ||
+          (playerSelection === "Scissors" && computerSelection === "Paper")) {
     playerWins++;
     rounds++;
-    return 'You win! You chose \"' + playerLower + '\" and the computer chose \"' + cpuLower + '.';
-  } else if((playerSelection === 'Rock' && computerSelection === 'Paper') ||
-          (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
-          (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+    return "You win! You chose " + playerLower + " and the computer chose " + cpuLower + ".";
+  } else if((playerSelection === "Rock" && computerSelection === "Paper") ||
+          (playerSelection === "Paper" && computerSelection === "Scissors") ||
+          (playerSelection === "Scissors" && computerSelection === "Rock")) {
     cpuWins++;
     rounds++;
-    return 'You lose... You chose ' + playerLower + ' and the computer chose ' + cpuLower + '.';
-  } else {
-    return 'You didn\'t choose rock, paper, or scissors. Or you made a typo.'
+    return "You lose... You chose " + playerLower + " and the computer chose " + cpuLower + ".";
   }
 }
-function rpsImage() {
-  document.getElementsByClassName("player-choice")[0].src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQtZhQB9qDQgJfBt_DOxaHFX1tIhnMSSbeEw&usqp=CAU"
 
-}
